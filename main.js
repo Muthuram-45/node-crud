f5417862c6cb48272e7661abc4940895b0a56a58
Muthuram-45 <muthuram921@gmail.com>
2025-05-20 21:02:49 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.json({ msg:"Hello World!" });
 })
 
+
+// Crud
+app.get('/movies',()=>{
+
+});
+
+app.post('/movies',()=>{
+
+});
+
+app.put('/movies/:id',()=>{
+
+});
+
+app.delete('/movies',()=>{
+
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 })
